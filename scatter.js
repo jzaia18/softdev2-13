f6d406e00a710e414f14a79e59fb89aa7a92488b
edmond120 <edmondwong888@msn.com>
2018-03-18 18:36:52 -0400

    done

--- scatter.js
+++ scatter.js
@@ -1,29 +1,45 @@
-var plot = document.getElementById("plot");
-var arcadeScale = 1; // a constant (CHANGE THIS)
-var csScale = 1; // a constant (CHANGE THIS)
+var arcadeScale = 100; // a constant (CHANGE THIS)
+var csScale = 1/10; // a constant (CHANGE THIS)
 
-var addPoint = function(x, y, yscale, color) {
-  // fill me in!
+//d3 select from id
+var plot = d3.select('#plot');
+
+//adding width and height
+plot.attr("width" ,"1000")
+plot.attr("height","1000")
+
+var addPoints = function(plot,data) {
+	plot.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx"   , function(a) { return (a[0] - 2000)*10 + 10; } )
+		.attr("cy"   , function(a) { return a[1] * a[2]; } )
+		.attr("r"    , function(a) { return 5;    } )
+		.attr("style", function(a) { return "fill: " + a[3]; } )
 };
 
-addPoint(2000, 1.19, arcadeScale, "red");
-addPoint(2001, 1.17, arcadeScale, "red");
-addPoint(2002, 1.27, arcadeScale, "red");
-addPoint(2003, 1.24, arcadeScale, "red");
-addPoint(2004, 1.31, arcadeScale, "red");
-addPoint(2005, 1.44, arcadeScale, "red");
-addPoint(2006, 1.60, arcadeScale, "red");
-addPoint(2007, 1.65, arcadeScale, "red");
-addPoint(2008, 1.80, arcadeScale, "red");
-addPoint(2009, 1.73, arcadeScale, "red");
+var Data = [
+	[2000, 1.19, arcadeScale, "red"],
+	[2001, 1.17, arcadeScale, "red"],
+	[2002, 1.27, arcadeScale, "red"],
+	[2003, 1.24, arcadeScale, "red"],
+	[2004, 1.31, arcadeScale, "red"],
+	[2005, 1.44, arcadeScale, "red"],
+	[2006, 1.60, arcadeScale, "red"],
+	[2007, 1.65, arcadeScale, "red"],
+	[2008, 1.80, arcadeScale, "red"],
+	[2009, 1.73, arcadeScale, "red"],
+	[2000,  861, csScale, "black"],
+	[2001,  830, csScale, "black"],
+	[2002,  809, csScale, "black"],
+	[2003,  867, csScale, "black"],
+	[2004,  948, csScale, "black"],
+	[2005, 1129, csScale, "black"],
+	[2006, 1453, csScale, "black"],
+	[2007, 1656, csScale, "black"],
+	[2008, 1787, csScale, "black"],
+	[2009, 1611, csScale, "black"],
+];
 
-addPoint(2000,  861, csScale, "black");
-addPoint(2001,  830, csScale, "black");
-addPoint(2002,  809, csScale, "black");
-addPoint(2003,  867, csScale, "black");
-addPoint(2004,  948, csScale, "black");
-addPoint(2005, 1129, csScale, "black");
-addPoint(2006, 1453, csScale, "black");
-addPoint(2007, 1656, csScale, "black");
-addPoint(2008, 1787, csScale, "black");
-addPoint(2009, 1611, csScale, "black");
+addPoints(plot,Data);
