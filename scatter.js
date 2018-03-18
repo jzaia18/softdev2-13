0e13d5c886de7194c72edb694b816658c6ce0267
Jake Zaia <jzaia@stuy.edu>
2018-03-18 17:27:12 -0400

    Basic layout

---
+++ scatter.js
@@ -0,0 +1,29 @@
+var plot = document.getElementById("plot");
+var arcadeScale = 1; // a constant (CHANGE THIS)
+var csScale = 1; // a constant (CHANGE THIS)
+
+var addPoint = function(x, y, yscale, color) {
+  // fill me in!
+};
+
+addPoint(2000, 1.19, arcadeScale, "red");
+addPoint(2001, 1.17, arcadeScale, "red");
+addPoint(2002, 1.27, arcadeScale, "red");
+addPoint(2003, 1.24, arcadeScale, "red");
+addPoint(2004, 1.31, arcadeScale, "red");
+addPoint(2005, 1.44, arcadeScale, "red");
+addPoint(2006, 1.60, arcadeScale, "red");
+addPoint(2007, 1.65, arcadeScale, "red");
+addPoint(2008, 1.80, arcadeScale, "red");
+addPoint(2009, 1.73, arcadeScale, "red");
+
+addPoint(2000,  861, csScale, "black");
+addPoint(2001,  830, csScale, "black");
+addPoint(2002,  809, csScale, "black");
+addPoint(2003,  867, csScale, "black");
+addPoint(2004,  948, csScale, "black");
+addPoint(2005, 1129, csScale, "black");
+addPoint(2006, 1453, csScale, "black");
+addPoint(2007, 1656, csScale, "black");
+addPoint(2008, 1787, csScale, "black");
+addPoint(2009, 1611, csScale, "black");
