c4dbd0ef62ba085b3c082f823ddc82ce5fd7e183
Jake Zaia <jzaia@stuy.edu>
2018-03-19 15:55:03 -0400

    Fix problems to close 2 issues

--- scatter.js
+++ scatter.js
@@ -2,11 +2,11 @@
 var plot = d3.select('#plot');
 
 var scaleX = function(n) {
-  return n * .45 - 320;
+  return n * .35 - 200;
 };
 
 var scaleY = function(n) {
-  return n * 100;
+  return 500 - n * 250;
 };
 
 var addPoints = function(plot, data) {
@@ -23,10 +23,21 @@ var addPoints = function(plot, data) {
 var addAxis = function() {
   //X
   plot.append("text")
-    .attr("x", 200 )
+    .attr("x", 170 )
     .attr("y", 400 )
     .text("Computer Science Doctorates awarded (US)");
+  for (var i = 800; i < 2000; i+=100) {
+    plot.append("text").attr("x", scaleX(i)).attr("y", 350).text(i);
+  }
   //Y
+  plot.append("text")
+    .attr("x", 20 )
+    .attr("y", 300 )
+    .attr("transform", "rotate(270 20 300)")
+    .text("Arcade Revenue (Billions of dollars)");
+  for (i = 70; i < 200; i+=10) {
+    plot.append("text").attr("x", 25).attr("y", scaleY(i/100)).text(i);
+  }
 };
 
 var data = [
