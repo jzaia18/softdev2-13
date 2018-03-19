8dd56957e4bf2c84a1939fabb4f8760d75c759d5
Jake Zaia <jzaia@stuy.edu>
2018-03-19 14:41:26 -0400

    Small update

--- scatter.js
+++ scatter.js
@@ -1,41 +1,46 @@
-var arcadeScale = 100; // a constant (CHANGE THIS)
-var csScale = 1/10; // a constant (CHANGE THIS)
-
 //d3 select from id
 var plot = d3.select('#plot');
 
-var addPoints = function(plot,data) {
-	plot.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("cx"   , function(a) { return (a[0] - 2000)*50 + 10; } )
-		.attr("cy"   , function(a) { return a[1] * a[2]; } )
-		.attr("r"    , function(a) { return 5;    } )
-		.attr("style", function(a) { return "fill: " + a[3]; } );
+var scaleX = function(n) {
+  return n * .45 - 320;
 };
 
-var Data = [
-	[2000, 1.19, arcadeScale, "red"],
-	[2001, 1.17, arcadeScale, "red"],
-	[2002, 1.27, arcadeScale, "red"],
-	[2003, 1.24, arcadeScale, "red"],
-	[2004, 1.31, arcadeScale, "red"],
-	[2005, 1.44, arcadeScale, "red"],
-	[2006, 1.60, arcadeScale, "red"],
-	[2007, 1.65, arcadeScale, "red"],
-	[2008, 1.80, arcadeScale, "red"],
-	[2009, 1.73, arcadeScale, "red"],
-	[2000,  861, csScale, "black"],
-	[2001,  830, csScale, "black"],
-	[2002,  809, csScale, "black"],
-	[2003,  867, csScale, "black"],
-	[2004,  948, csScale, "black"],
-	[2005, 1129, csScale, "black"],
-	[2006, 1453, csScale, "black"],
-	[2007, 1656, csScale, "black"],
-	[2008, 1787, csScale, "black"],
-	[2009, 1611, csScale, "black"],
+var scaleY = function(n) {
+  return n * 100;
+};
+
+var addPoints = function(plot, data) {
+  plot.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx"   , function(a) { return scaleX(a[0]); } )
+    .attr("cy"   , function(a) { return scaleY(a[1]); } )
+    .attr("r"    , 5 )
+    .attr("fill" , "black" );
+};
+
+var addAxis = function() {
+  //X
+  plot.append("text")
+    .attr("x", 200 )
+    .attr("y", 400 )
+    .text("Computer Science Doctorates awarded (US)");
+  //Y
+};
+
+var data = [
+  [ 861, 1.19, 2000],
+  [ 830, 1.17, 2001],
+  [ 809, 1.27, 2002],
+  [ 867, 1.24, 2003],
+  [ 948, 1.31, 2004],
+  [1129, 1.44, 2005],
+  [1453, 1.60, 2006],
+  [1656, 1.65, 2007],
+  [1787, 1.80, 2008],
+  [1611, 1.73, 2009],
 ];
 
-addPoints(plot,Data);
+addAxis();
+addPoints(plot, data);
